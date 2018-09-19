cebddcf859b6c4522be0040537cf071d0c00a0ea
vincent.wen <wenwei1202@gmail.com>
2018-09-19 15:51:33 +0800

    remove dist

--- dist/src/SecurityCheck.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class MuteSecurityCheck {
-    async isValidRedirectUri(..._) {
-        return true;
-    }
-}
-exports.MuteSecurityCheck = MuteSecurityCheck;
