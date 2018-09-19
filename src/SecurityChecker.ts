240bd1dfc5f1cdb835a0a747d7b5e51e6ba19fc8
vincent.wen <wenwei1202@gmail.com>
2018-09-19 15:53:44 +0800

    refactoring

--- src/SecurityChecker.ts
+++ src/SecurityChecker.ts
@@ -1,5 +1,5 @@
-import {List} from 'immutable'
-import {URL} from 'url'
+import { List } from 'immutable'
+import { URL } from 'url'
 export interface ISecurityChecker {
   isValidRedirectUri(uri: string): boolean
 }
@@ -7,32 +7,21 @@ export interface ISecurityChecker {
 export class SimpleSecurityChecker implements ISecurityChecker {
   private whitelist: List<string>
   constructor(validHosts: string[]) {
-    const regex = /^((\w|-)+\.)+(\w|-)+(:\d+)?$/
-    const list = List<string>()
-    this.whitelist = list.withMutations((mutable) => {
-      for (const host of validHosts) {
-        if (!regex.test(host)) {
-          throw new Error(`Invalid host:${host}.`)
-        } else {
-          if (host.endsWith(':80')) {
-            mutable.push(host.split(':')[0])
-          } else {
-            mutable.push(host)
-          }
-        }
-      }
+    const hosts = validHosts.map((host) => {
+      const url = new URL(host)
+      return url.hostname
     })
+    this.whitelist = List(hosts)
   }
   public isValidRedirectUri(uri: string) {
-    const host = new URL(uri).host
-    const valid = this.whitelist.some((item) => item === host)
-    return valid
+    const host = new URL(uri).hostname
+    return this.whitelist.some((item) => item === host)
   }
 }
 
 // tslint:disable-next-line:max-classes-per-file
 export class MuteSecurityChecker implements ISecurityChecker {
-  public isValidRedirectUri(_: string) {
+  public isValidRedirectUri() {
     return true
   }
 }
